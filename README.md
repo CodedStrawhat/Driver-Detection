10dc06c56a6264e74653598b582e0d9c9da2ec84
CodedStrawhat <vspanwar1@gmail.com>
2022-03-25 21:59:33 +0530

    Using mediapipe instead of dlib

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Driver-Detection
 
-Download landmark data from 
-http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+
--- hacktag.py
+++ hacktag.py
@@ -1,23 +1,21 @@
 #Importing required libraries
 import playsound               
-from imutils import face_utils 
 from threading import Thread
-import subprocess
 import numpy as np
-import imutils
-import time
-import dlib
 import cv2 as cv
+import mediapipe as mp
+from scipy.spatial import distance as dis
 
 cap = cv.VideoCapture(0)
 
-detector = dlib.get_frontal_face_detector() #pre trained face detector model
-predictor = dlib.shape_predictor("landmarks_data.dat") #face landmarks data (see README for download link)
+#Mediapipe's face classification model
+face_mesh = mp.solutions.face_mesh
+face_model = face_mesh.FaceMesh(static_image_mode = False,refine_landmarks = True)
 
 #Eye aspect ratio 
 def EAR(eye):
-    num = np.linalg.norm(eye[1]-eye[5]) + np.linalg.norm(eye[2]-eye[4])
-    denom = np.linalg.norm(eye[0]-eye[3])*2.0
+    num = dis.euclidean(eye[2],eye[3])
+    denom = dis.euclidean(eye[0],eye[1])
     ear = num/denom
     return ear
 
@@ -29,54 +27,67 @@ def sound(path):
 
 counter = 0 #To count number of frames
 alarm = 0 #Whether to play the alarm sound or not
+a = 0
 while True:
+
     isTrue,frame = cap.read()
     if isTrue == 0:
         break 
-    
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    faces = detector(gray) #Detecting faces in gray image of the current frame
-    for face in faces:
 
-        #Finding landmarks for right and left eye
-        landmarks = predictor(gray,face) 
-        landmarks = face_utils.shape_to_np(landmarks)
-        left_eye = np.array(landmarks[36:42])
-        right_eye = np.array(landmarks[42:48])
+    rgb_im = cv.cvtColor(frame, cv.COLOR_BGR2RGB) #Converting image to rgb
 
-        #Calculating EAR for left and right eye and average EAR
-        leftEAR = EAR(left_eye)
-        rightEAR = EAR(right_eye)
-        ear = (leftEAR+rightEAR)/2.0
+    #Finding landmarks for right and left eye
+    output = face_model.process(rgb_im)
+    output = output.multi_face_landmarks[0]
+    l = []
+    for landmark in output.landmark:
+        x = (landmark.x)*frame.shape[1] #Denormalizing points
+        y = (landmark.y)*frame.shape[0]
+        l.append([x,y])
+    l = np.array(l,dtype = int)
 
-        #Drawing contours over eyes
-        leftHull =  cv.convexHull(left_eye)
-        rightHull =  cv.convexHull(right_eye)
-        cv.drawContours(frame, [leftHull], -1, (0, 255, 0), 1)
-        cv.drawContours(frame, [rightHull], -1, (0, 255, 0), 1)
+    #Indexes of landmarks of left and right eye
+    left_eye = [ 362, 382, 381, 380, 374, 373, 390, 249, 263, 466, 388, 387, 386, 385,384, 398 ]
+    right_eye = [ 33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161 , 246 ]
+    left_eye = np.array(l[left_eye[:]][:],dtype = int)
+    right_eye = np.array(l[right_eye[:]][:],dtype = int)
 
-        #If EAR is less than threshold value then eyes are closed
-        if (ear<0.27):
-            counter+=1
-            if(counter>=48):    #If eyes are closed for 48 frames or more
+    #Points required to find EAR
+    left_points = np.array(l[[386,374,263,362]][:],dtype = int)
+    right_points = np.array(l[[159, 145,133,33]][:], dtype = int)
 
-                #Making a thread object and starting it to play the sound if not aldready playing
-                if(alarm == 0):
-                    alarm = 1
-                    t = Thread(target=sound,args = ('beep.wav',)) #Replace beep.wav with whatever sound you have saved
-                    s = 1
-                    t.start()
+    #Calculating EAR for left and right eye and average EAR
+    leftEAR = EAR(left_points)
+    rightEAR = EAR(right_points)
+    ear = (leftEAR+rightEAR)/2.0
 
-                cv.putText(frame, "DROWSINESS ALERT!", (10, 30),
-				cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        else:
-            #End the sound thread if eyes are no longer closed
-            if(alarm==1):
-                s = 0
-                t.join()
-            counter = 0
-            alarm = 0
-            
+    #Drawing contours over eyes
+    leftHull =  cv.convexHull(left_eye)
+    rightHull =  cv.convexHull(right_eye)
+    cv.drawContours(frame, [leftHull], -1, (0, 255, 0), 1)
+    cv.drawContours(frame, [rightHull], -1, (0, 255, 0), 1)
+
+    #If EAR is more than threshold value then eyes are considered closed
+    if (ear>5):
+        counter+=1
+        if(counter>=48):    #If eyes are closed for 48 frames or more
+
+            #Making a thread object and starting it to play the sound if not aldready playing
+            if(alarm == 0):
+                alarm = 1
+                t = Thread(target=sound,args = ('beep.wav',)) #Replace beep.wav with whatever sound you have saved
+                s = 1
+                t.start()
+
+            cv.putText(frame, "DROWSINESS ALERT!", (10, 30),
+            cv.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    else:
+        #End the sound thread if eyes are no longer closed
+        if(alarm==1):
+            s = 0
+            t.join()
+        counter = 0
+        alarm = 0
     cv.imshow('Video', frame)
     key = cv.waitKey(1)
     if key == 27:
